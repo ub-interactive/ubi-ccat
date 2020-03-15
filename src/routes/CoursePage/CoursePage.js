9748d859731eab48b21e48fab490af94ca94a2f0
liao0007 <t3dBear001>
2020-03-16 00:26:18 +0800

    update

--- src/routes/CoursePage/CoursePage.js
+++ src/routes/CoursePage/CoursePage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './CoursePage.css';
+import {Link} from "react-router-dom";
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 class CoursePage extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            currentTab: 0,
+            fixedTabBar: false
+        }
     }
 
     componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+
         const url = `http://10.0.0.5:9000/api/web/c/${this.props.match.params.courseId}`;
         fetch(url)
             .then(response => response.json())
@@ -16,25 +24,35 @@ class CoursePage extends React.Component {
             })));
     }
 
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const cInfo = document.getElementsByClassName("c-info")[0];
+        const threshold = cInfo.offsetHeight + cInfo.offsetTop;
+        this.setState({
+            fixedTabBar: window.pageYOffset >= threshold
+        })
+    };
+
     render() {
-        return <div className="p-c">
+        return this.state.courseId === undefined ? <div>加载中</div> : <div className="p-c">
             <div className="co-scroll-view">
                 <div className="banner">
                     <picture className="c-media-blur-bg co-ximg-cover">
-                        <img
-                            src="https://assets.xrkmm.cn/u/3000000001831909e3653a34-038a-4222-b7e0-058fc9310fd8.jpg?x-oss-process=image/resize,m_fill,w_1125,h_750,limit_0"/>
+                        <img src={this.state.coverUrl}/>
                     </picture>
                     <div className="co-media-player co-media-player-audio co-media-player-init">
                         <picture className="co-media-player-cover">
-                            <img
-                                src="https://assets.xrkmm.cn/u/3000000001831909e3653a34-038a-4222-b7e0-058fc9310fd8.jpg?x-oss-process=image/resize,m_fill,w_1125,h_750,limit_0"/>
+                            <img src={this.state.coverUrl}/>
                         </picture>
                         <div className="btn-play-mask">
-                            <div className="btn-media-play"></div>
+                            <div className="btn-media-play"/>
                         </div>
                         <div className="audio-disk">
                             <div className="audio-disk-ent"
-                                 style={{"background-image": "url(&quot;https://assets.xrkmm.cn/u/3000000001831909e3653a34-038a-4222-b7e0-058fc9310fd8.jpg?x-oss-process=image/resize,m_fill,w_1125,h_750,limit_0&quot;)"}}></div>
+                                 style={{"background-image": `url(&quot;${this.state.coverUrl}&quot;)`}}/>
                         </div>
                     </div>
                 </div>
@@ -42,43 +60,45 @@ class CoursePage extends React.Component {
                     <div className="main-info">
                         <div className="left">
                             <div className="info-name">
-                                <div className="c-title2">心理学家周梵：15节课带你戒除孩子手机瘾/网瘾，收获健康亲子关系！</div>
-                                <div className="subname">科学根治孩子手机瘾/网瘾</div>
+                                <div className="c-title2">{this.state.title}</div>
+                                <div className="subname">{this.state.subtitle}</div>
                             </div>
                             <div className="tag-list">
-                                <div className="c-course-tag-item">课程永久有效</div>
+                                {this.state.tags.map(tag => <div key={tag} className="c-course-tag-item">{tag}</div>)}
                             </div>
                         </div>
                         <div className="right">
                             <div className="info-money">
-                                <div className="cur">￥<b>99</b></div>
-                                <div className="origin">原价￥<span style={{"text-decoration": "line-through"}}>129</span>
+                                <div className="cur">￥<b>{this.state.promotionPrice / 100}</b></div>
+                                <div className="origin">原价￥<span
+                                    style={{"text-decoration": "line-through"}}>{this.state.price / 100}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="co-tab-view panel fixed">
+                <div className={`co-tab-view panel ${this.state.fixedTabBar ? "fixed" : ""}`}>
                     <div className="co-tab-view-tabs">
-                        <div className="co-tab-view-tab current"><span>课程介绍</span></div>
-                        <div className="co-tab-view-tab"><span>课程目录</span></div>
-                        <div className="co-tab-view-tab"><span>课程须知</span></div>
+                        <a href="#course-intro"
+                              className={`co-tab-view-tab ${this.state.currentTab === 0 ? "current" : ""}`}
+                              onClick={() => this.setState({currentTab: 0})}><span>课程介绍</span></a>
+                        <a href="#course-menu"
+                              className={`co-tab-view-tab ${this.state.currentTab === 1 ? "current" : ""}`}
+                              onClick={() => this.setState({currentTab: 1})}><span>课程目录</span></a>
+                        <a href="#course-info"
+                              className={`co-tab-view-tab ${this.state.currentTab === 2 ? "current" : ""}`}
+                              onClick={() => this.setState({currentTab: 2})}><span>课程须知</span></a>
                     </div>
                 </div>
-                <div className="intro-details"/>
-                <div className="co-c-intro">
-                    {this.props.courseIntro}
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                </div>
-                <div className="intro-dir">
-                    {this.props.courseMenu}
-                </div>
-                <div className="intro-item intro-notice">
-                    {this.props.courseInfo}
-                </div>
+                <ScrollableAnchor id="course-intro">
+                    <div className="course-intro" dangerouslySetInnerHTML={{__html: this.state.courseIntro}}/>
+                </ScrollableAnchor>
+                <ScrollableAnchor id="course-menu">
+                    <div className="course-menu" dangerouslySetInnerHTML={{__html: this.state.courseMenu}}/>
+                </ScrollableAnchor>
+                <ScrollableAnchor id="course-info">
+                    <div className="course-info" dangerouslySetInnerHTML={{__html: this.state.courseInfo}}/>
+                </ScrollableAnchor>
                 <div className="footer-placeholder"/>
             </div>
             <div className="footer">
