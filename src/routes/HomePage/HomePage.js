3588f595ce3fcec40e97f41a8c06a403dcd60293
liao0007 <t3dBear001>
2020-03-20 03:11:52 +0800

    update

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -43,6 +43,7 @@ class HomePage extends React.Component {
         // this.userService.getWechatUserInfo(this.props.location,userInfo => {
         //     this.wsService.createUser("18600094776", userInfo.openId, alert("user created"));
         // });
+        console.log(123);
         this.wsService.homePageGetInfo(data => {
             this.setState({...data});
             this.loadMore()
